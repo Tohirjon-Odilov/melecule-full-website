10affcc8be2e7a8c3fd278ea4f3e51b8b9164dfe
Shahanshoh03 <82634626+Shahanshoh03@users.noreply.github.com>
2022-03-23 00:04:50 +0300

    tekshirish uchun joylandi

---
+++ script.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLikns = document.querySelector('.navbar_menu')
+const menuBtn = document.querySelector('#button')
+let bg = document.querySelector("#nav")
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLikns.classList.toggle('active');
+    menuBtn.classList.toggle('active');
+    // bg.classList.add("bg")
+})
